200a177f78d11fd9663b6541c27432ff0cae2921
Giovanna Becker <86936125+Giobecker@users.noreply.github.com>
2022-01-30 18:26:32 -0300

    Add files via upload

---
+++ sistema que mostra maior número.py
@@ -0,0 +1,14 @@
+number1 = int(input("Digite o primeiro número: "))
+number2 = int(input("Digite o segundo número: "))
+
+if number1 > number2 :
+    print("Primeiro número é maior que o segundo")
+elif number1== number2:
+    print("Números iguals")
+else:
+     print("Numeros diferetes")
+    
+    
+            
+     
+
